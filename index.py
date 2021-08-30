06c4a7690353015bd5e09105887dd9c07b1d7d07
Lucas Santus <leos9877@gmail.com>
2021-08-30 09:42:52 -0300

    :pushpin: Implementando códigos.  - Foi implementado código do projeto.  - Foi adicionado novas imagens ao sistema.  - Foi desenvolvido estrutura de erros para o sistema.  - Foi montado uma exibição, onde somente as imagens concluídas serão mostradas no terminal.

--- index.py
+++ index.py
@@ -17,7 +17,18 @@ list_links = [
     "https://source.unsplash.com/8mUEy0ABdNE/800x600",
     "https://source.unsplash.com/G9Rfc1qccH4/800x600",
     "https://source.unsplash.com/aJeH0KcFkuc/800x600",
-    "https://source.unsplash.com/p2TQ-3Bh3Oo/800x600", 
+    "https://source.unsplash.com/p2TQ-3Bh3Oo/800x600",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtT_Okoy-bdhjWA9QPv1tXdtoRQd7P6rOXEg&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3X7iiJcDpD-QP6ln5kJmUzRbJH3Tgn5zWhg&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ34iEOaXace3VrMFdmWGZlPt_tA45DhQgVUA&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4-2xeXvpasKZAw31lKDmXlwDHS6IRYJmOsg&usqp=CAU",
+    "https://www.hypeness.com.br/1/2018/12/imagens-surreais7.jpg",
+    "https://img.cancaonova.com/cnimages/canais/uploads/sites/6/2019/11/formacao_1600x1200-imagens-e-sons-de-cada-dia.jpg",
+    "https://marketingcomcafe.com.br/wp-content/uploads/2018/01/banco_de_imagens_menino.jpg",
+    "https://static5.depositphotos.com/1003371/521/i/950/depositphotos_5214674-stock-photo-tropical-sea-sunset.jpg",
+    "https://marketingcomcafe.com.br/wp-content/uploads/2018/01/ladi-storing-music-2-1.jpg",
+    "https://marketingcomcafe.com.br/wp-content/uploads/2018/01/banco_de_imagens_free.jpeg",
+    "https://marketingcomcafe.com.br/wp-content/uploads/2017/12/banco-de-imagens-morguefile.png",
 ]
 
 # Função para recuperar imagem
@@ -29,7 +40,11 @@ def get_image(url, save_image=''):
 
 # Percorrendo lista de links
 for link in list_links:
-    name = f"images/image-{ list_links.index(link) }"
-    dir = f"{name}.jpg"
-    image = get_image(link, dir)
-    cv2.imwrite(dir, image)
+    try:
+        name = f"images/image-{ list_links.index(link) }"
+        dir = f"{name}.jpg"
+        image = get_image(link, dir)
+        cv2.imwrite(dir, image)
+        print(f'"{link}",')
+    except:
+        pass
